f6d17d18a9fe81772257fa34b33d8aa5d2d301c7
Mike Xu <mxu11290@gmail.com>
2018-10-10 16:04:17 -0400

    get rid of semicolons

--- index.d.ts
+++ index.d.ts
@@ -2,35 +2,35 @@
  * Types should be included in did-resolver package
  */
 interface DIDDoc {
-  '@context': 'https://w3id.org/did/v1';
-  id: string;
-  publicKey: PublicKey[];
-  authentication: Authentication[];
+  '@context': 'https://w3id.org/did/v1'
+  id: string
+  publicKey: PublicKey[]
+  authentication: Authentication[]
 }
 
 interface PublicKey {
-  id: string;
-  type: string;
-  owner: string;
-  ethereumAddress?: string;
+  id: string
+  type: string
+  owner: string
+  ethereumAddress?: string
 }
 
 interface Authentication {
-  type: string;
-  publicKey: string;
+  type: string
+  publicKey: string
 }
 
 interface ParsedDID {
-  did: string;
-  method: string;
-  id: string;
+  did: string
+  method: string
+  id: string
 }
 
 declare module 'did-resolver' {
   export function registerMethod(
     method: string,
     resolver: (did: string, parsed: ParsedDID) => Promise<DIDDoc | null>,
-  ): void;
-  export function parse(did: string): any;
-  export default function resolve(did: string): Promise<DIDDoc | null>;
+  ): void
+  export function parse(did: string): any
+  export default function resolve(did: string): Promise<DIDDoc | null>
 }
